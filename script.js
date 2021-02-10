288c0ace11d5921907a9f75a37f967753a156368
Cameron Frost <frost@Camerons-MBP.attlocal.net>
2021-02-10 11:12:08 -0600

    Beer description

--- script.js
+++ script.js
@@ -10,21 +10,27 @@ async function getBeers() {
     const beersDiv = document.querySelector('.beers');
     
     let beerHtml = "";
-    
-    // Add the rest of the beer data to the HTML template.
-    
-    console.log(beers[0]);
+      
+    // Fill in the blanks with the rest of the data
     
     beers.forEach(beer => {
        beerHtml += `
         <div class='beer-wrapper card'>
             <div class='beer'>
-                <img class='beer__img' src="">
+                <img class='beer__img' src="${beer.image_url}">
                 <h3>${beer.name}</h3>
                 <span class='beer__info'>
-                    <span>ABV: %</span>
-                    <span>IBU: </span>
+                    <span>ABV: ${beer.abv}%</span>
+                    <span>IBU: ${beer.ibu}</span>
                 </span>
+            </div>
+            <div class='beer__content'>
+                <div class='beer__name'>${beer.name}</div>
+                <div class='beer__tagline'>${beer.tagline}</div>
+                <div class='beer__description'>${beer.description}</div>
+                <div class='beer__food-pairing'>
+                    Pair with: ${beer.food_pairing.join(', ')}
+                </div>
             </div>
         </div>
        `; 
