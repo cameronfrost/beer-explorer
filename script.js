32e4583b7be50668953926fd40d736d01367ca74
Cameron <c@cameronfrost.com>
2021-02-11 13:13:25 -0600

    Filter by IBU

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // variables
 const urlBase = "https://api.punkapi.com/v2/beers";
 const filterABV = document.getElementById("filterABV");
-let optionsABV = "";
+const filterIBU = document.getElementById("filterIBU");
+let optionsABV = "", optionsIBU = "";
 
 // filters
 filterABV.addEventListener("change", e => {
@@ -25,8 +26,29 @@ filterABV.addEventListener("change", e => {
    getBeers();
 });
 
+filterIBU.addEventListener("change", e => {
+   const value = e.target.value; 
+   
+   switch (value) {
+        case "all":
+            optionsIBU = "";
+            break
+        case "weak":
+            optionsIBU = "ibu_lt=35";
+            break
+        case "medium":
+            optionsIBU = "ibu_gt=34&ibu_lt=75";
+            break
+        case "strong":
+            optionsIBU = "ibu_gt=74";
+            break
+   }
+   
+   getBeers();
+});
+
 async function getBeers() {
-    const url = urlBase + "?" + optionsABV;
+    const url = urlBase + "?" + optionsABV + "&" + optionsIBU;
     // fetch
     const beerPromise = await fetch(url);
     const beers = await beerPromise.json();
